5a3d31b964d75fea5b3726cc0ecf34068fae9897
Denis Pitsul <denia.pitsul@gmail.com>
2025-05-05 17:43:38 +0300

    make build

---
+++ index.7c2ab8e9.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var n=e.target.closest(".list-item__link");n&&(e.preventDefault(),t.src=n.href)});
+//# sourceMappingURL=index.7c2ab8e9.js.map
